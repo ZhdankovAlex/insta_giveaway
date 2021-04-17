3017c588ce37999b02469f3ad6277d34466aa582
ZhdankovAlex <47736968+ZhdankovAlex@users.noreply.github.com>
2021-04-17 21:35:53 +0300

    let's do it

--- instaway/core/views.py
+++ instaway/core/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
 from django.http import *
 from instabot import Bot
+
+def index(request):
+    return render(request, "index.html")
+
+def main(request):
+
+    username = request.GET.get("name", "Undefined user")
+    profile_icon = request.GET.get("avatar", "user.jpg")
+    data = {"profile_icon": profile_icon, "username": username}
+    return render(request, "main.html", context=data)
 
 # статусные коды
 def m304(request):
@@ -24,41 +33,3 @@ def m410(request):
 
 def m500(request):
     return HttpResponseServerError("<h2>Something is wrong</h2>")
-
-# переадресация на главную страницу
-def index(request):
-    return render(request, 'insta/main.html')
-
-# главная страница
-def main(request):
-    return render(request, 'insta/main.html')
-
-# политика конфиденциальности
-def privacy_policy(request):
-    return render(request, 'insta/privacy_policy.html')
-
-# авторизированный пользователь
-@login_required
-def authorized(request):
-    return render(request, 'insta/authorized.html')
-
-# страница админа (надо чекать админскую авторизацию,
-# не должно быть возможности зайти простому смертному)
-@login_required
-def admin_page(request):
-    return render(request, 'insta/admin_page.html')
-
-# страничка с посчитанными лайками/репостами/хэштегами
-@login_required
-def link_parse(request):
-    return render(request, 'insta/link_parse.html')
-
-# результаты розыгрыша
-@login_required
-def results(request):
-    return render(request, 'insta/results.html')
-
-# прогрессбар
-@login_required
-def progress_bar(request):
-    return render(request, 'insta/progress_bar.html')
--- instaway/instaway/urls.py
+++ instaway/instaway/urls.py
@@ -1,13 +1,10 @@
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path
 from core import views
 
 urlpatterns = [
-    re_path(r'^authorized/link_parse/', views.link_parse, name="link_parse"),
-    re_path(r'^authorized/progress_bar/', views.progress_bar, name="progress_bar"),
-    re_path(r'^authorized/results/', views.results, name="results"),
-    re_path(r'^authorized/', views.authorized, name="authorized"),
-    re_path(r'^$', views.main, name="main"),
+    path('/', views.index),
+    path('main/', views.main),
     # встроенная админка
     path('admin/', admin.site.urls),
     path('logout/', views.main, name="main"),
